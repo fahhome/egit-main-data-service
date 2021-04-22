d16c7f81f8230a5f34613471628f838ccc2af032
fahhome <fahmidjobs@gmail.com>
2021-04-22 21:31:48 +0530

    Loading initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO airport (id,name,city_name,code,latitude,longitude) VALUES (1,'CSM International Airport', 'Mumbai', 'BOM' , 25.1510, 55.2152);
+INSERT INTO airport (id,name,city_name,code,latitude,longitude) VALUES (2,'Dubai International Airport', 'Dubai', 'DXB' , 25.1510, 55.2152);
+INSERT INTO airport (id,name,city_name,code,latitude,longitude) VALUES (3,'JFK International Airport', 'New York', 'JFK' , 40.7823, 73.1231);
+INSERT INTO airport (id,name,city_name,code,latitude,longitude) VALUES (4,'G. Bush International Airport', 'Houston', 'IAH' , 29.7823, 95.1231);
