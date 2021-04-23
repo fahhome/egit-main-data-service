b76cc9f5ab5737bc3aeb2eca8ccd8c9d248c8bb6
fahhome <fahmidjobs@gmail.com>
2021-04-23 16:38:01 +0530

    Minor change

--- src/main/java/fahmid/islam/egitasignment/controller/AirportController.java
+++ src/main/java/fahmid/islam/egitasignment/controller/AirportController.java
@@ -3,6 +3,7 @@ package fahmid.islam.egitasignment.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import fahmid.islam.egitasignment.entity.*;
 import fahmid.islam.egitasignment.service.*;
 
 @RestController
+@CrossOrigin
 public class AirportController {
     @Autowired
     private AirportService airportService;
