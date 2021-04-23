a3579f139941bfb29bfe56681d97a4ce96d3ecdf
fahhome <fahmidjobs@gmail.com>
2021-04-23 17:15:50 +0530

    Wrong marker Location Fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO airport (id,name,city_name,code,latitude,longitude) VALUES (1,'CSM International Airport', 'Mumbai', 'BOM' , 19.0519, 72.5205);
 INSERT INTO airport (id,name,city_name,code,latitude,longitude) VALUES (2,'Dubai International Airport', 'Dubai', 'DXB' , 25.1510, 55.2152);
-INSERT INTO airport (id,name,city_name,code,latitude,longitude) VALUES (3,'JFK International Airport', 'New York', 'JFK' , 40.3823, 73.4644);
-INSERT INTO airport (id,name,city_name,code,latitude,longitude) VALUES (4,'G. Bush International Airport', 'Houston', 'IAH' , 29.5904, 95.2029);
+INSERT INTO airport (id,name,city_name,code,latitude,longitude) VALUES (3,'JFK International Airport', 'New York', 'JFK' , 40.3823, -73.4644);
+INSERT INTO airport (id,name,city_name,code,latitude,longitude) VALUES (4,'G. Bush International Airport', 'Houston', 'IAH' , 29.5904, -95.2029);
